31d96e1d3cfdb3841f3971e408b817266d4465bd
lareii <lareii@proton.me>
2025-01-13 14:42:31 +0300

    fix: norminette rules

--- src/main.c
+++ src/main.c
@@ -6,22 +6,18 @@
 /*   By: ebabaogl <ebabaogl@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/12 19:39:45 by ebabaogl          #+#    #+#             */
-/*   Updated: 2025/01/13 13:53:37 by ebabaogl         ###   ########.fr       */
+/*   Updated: 2025/01/13 14:41:58 by ebabaogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-#include "libft.h"
 
 int	main(int argc, char **argv)
 {
 	t_vars	vars;
 
 	if (argc != 2)
-	{
-		ft_putendl_fd("Invalid arguments.\nUsage: ./fdf <map>", 2);
 		return (1);
-	}
 	if (init_map(&vars, argv[1]) == -1)
 		return (1);
 	if (init_win(&vars) == -1)
--- src/map/read_map.c
+++ src/map/read_map.c
@@ -6,7 +6,7 @@
 /*   By: ebabaogl <ebabaogl@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 10:15:51 by ebabaogl          #+#    #+#             */
-/*   Updated: 2025/01/13 13:57:10 by ebabaogl         ###   ########.fr       */
+/*   Updated: 2025/01/13 14:39:55 by ebabaogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,45 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+static int	read_file(int fd, char **map, char **buf)
+{
+	ssize_t	r_bytes;
+
+	r_bytes = 1;
+	*map = NULL;
+	while (r_bytes)
+	{
+		*buf = ft_calloc(1, BUFFER_SIZE + 1);
+		if (!*buf)
+		{
+			free(*map);
+			return (-1);
+		}
+		r_bytes = read(fd, *buf, BUFFER_SIZE);
+		if (r_bytes == -1)
+		{
+			free(*map);
+			free(*buf);
+			return (-1);
+		}
+		(*buf)[r_bytes] = '\0';
+		*map = ft_strjoin(*map, *buf);
+		if (!*map)
+			return (-1);
+	}
+	return (0);
+}
+
 char	*get_raw_map(char *filename)
 {
 	char	*buf;
 	char	*map;
-	int		r_bytes;
 	int		fd;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (NULL);
-	r_bytes = 1;
-	map = NULL;
-	while (r_bytes)
-	{
-		buf = ft_calloc(1, BUFFER_SIZE);
-		if (!buf)
-			return (free(map), NULL);
-		r_bytes = read(fd, buf, BUFFER_SIZE);
-		if (r_bytes == -1)
-			return (free(map), NULL);
-		buf[r_bytes] = '\0';
-		map = ft_strjoin(map, buf);
-		if (!map)
-			return (NULL);
-	}
+	if (read_file(fd, &map, &buf) == -1)
+		return (NULL);
 	return (map);
 }
